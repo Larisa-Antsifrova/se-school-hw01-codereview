2f49dcc8b83f5dfde511430a663c8b43ae7e4d02
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-08-04 03:31:47 +0300

    Refactored User repository.

--- repositories/users-repository.js
+++ repositories/users-repository.js
@@ -4,42 +4,48 @@ const { v4: uuidv4 } = require('uuid');
 
 const usersPath = path.join(__dirname, '..', 'db', 'users.json');
 
-const getAllUsers = async usersPath => {
-  try {
-    return JSON.parse(await fs.readFile(usersPath, 'utf-8'));
-  } catch (error) {
-    console.log('Error in getAllUsers: ', error.message);
+class User {
+  constructor(path) {
+    this.usersPath = path;
   }
-};
 
-const getUserByEmail = async email => {
-  try {
-    return (await getAllUsers(usersPath)).find(user => user.email === email);
-  } catch (error) {
-    console.log('Error in getUserByEmail: ', error.message);
+  async getAllUsers() {
+    try {
+      return JSON.parse(await fs.readFile(this.usersPath, 'utf-8'));
+    } catch (error) {
+      console.log('Error in getAllUsers: ', error.message);
+    }
   }
-};
 
-const addNewUser = async ({ name, email, password }) => {
-  try {
-    const id = uuidv4();
-
-    const newUser = {
-      id,
-      name,
-      email,
-      password,
-    };
-
-    const allUsers = await getAllUsers(usersPath);
-    allUsers.push(newUser);
-
-    await fs.writeFile(usersPath, JSON.stringify(allUsers, null, 2));
-
-    return { id, name, email };
-  } catch (error) {
-    console.log('Error in addNewUser: ', error.message);
+  async getUserByEmail(email) {
+    try {
+      return (await this.getAllUsers()).find(user => user.email === email);
+    } catch (error) {
+      console.log('Error in getUserByEmail: ', error.message);
+    }
   }
-};
 
-module.exports = { getAllUsers, getUserByEmail, addNewUser };
+  async addNewUser({ name, email, password }) {
+    try {
+      const id = uuidv4();
+
+      const newUser = {
+        id,
+        name,
+        email,
+        password,
+      };
+
+      const allUsers = await this.getAllUsers();
+      allUsers.push(newUser);
+
+      await fs.writeFile(this.usersPath, JSON.stringify(allUsers, null, 2));
+
+      return { id, name, email };
+    } catch (error) {
+      console.log('Error in addNewUser: ', error.message);
+    }
+  }
+}
+
+module.exports = new User(usersPath);
